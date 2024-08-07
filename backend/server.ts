858ce057ac2caf4579edeb8513e89cf47b16bc55
Thimira Manujaya <128280023+thimiramanujaya@users.noreply.github.com>
2024-08-07 12:33:54 +0530

    connected mysql database and tested

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,31 @@
-console.log('setup success!')
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    synchronize: true
+
+})
+
+const connectDb = async () => {
+
+    try {
+        const connection = await dataSource.initialize()
+
+        if(!connection) {
+            return console.log('Database not connected')
+        }
+        console.log('Database connected')
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error('Database connection failed')
+    }
+
+}
+
+connectDb()
